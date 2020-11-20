db9745ab15ddcbde1aa3c0096288f89ab64b286b
saerx <saerlaith.robyn@gmail.com>
2020-11-20 15:42:50 +0000

    first commit, directories installed and testclass written

---
+++ app/tests/player_test.py
@@ -0,0 +1,12 @@
+import unittest
+from app.models.player import Player
+
+class TestPlayer(unittest.TestCase):
+
+    def setUp(self):
+
+        player_1 = Player("Greg", "rock")
+
+    def test_player_has_name(self):
+        self.assertEqual("Greg", self.player_1.name)
+
