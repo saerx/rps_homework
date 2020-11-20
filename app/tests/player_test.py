af78fd4d98d417753e551c1d9888e6b58da03069
saerx <saerlaith.robyn@gmail.com>
2020-11-20 15:44:05 +0000

    made test player has name operational

--- app/tests/player_test.py
+++ app/tests/player_test.py
@@ -5,7 +5,7 @@ class TestPlayer(unittest.TestCase):
 
     def setUp(self):
 
-        player_1 = Player("Greg", "rock")
+        self.player_1 = Player("Greg", "rock")
 
     def test_player_has_name(self):
         self.assertEqual("Greg", self.player_1.name)
